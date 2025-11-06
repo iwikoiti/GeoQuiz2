7672c5ccd8e00da44f72cf8ce04d54b264fe5511
yui <yuineor@gmail.com>
2025-11-06 17:46:40 +0700

    add question and buttons

--- app/src/main/java/com/example/geoquiz2/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz2/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.geoquiz2.ui.theme.GeoQuiz2Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +43,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GeoQuizScreen(modifier: Modifier = Modifier) {
+fun QuizText(message: String, fontSize: Float){
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+    )
+}
 
+@Composable
+fun GeoQuizScreen(modifier: Modifier = Modifier) {
+    val questions = remember { QuizRepository.getQuestions() }
+    var currentIndex by remember { mutableStateOf(0) }
+    val currentQuestion = questions[currentIndex]
+
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 250.dp, start = 16.dp, end = 16.dp)
+    )
+    {
+        QuizText(message = currentQuestion.text, fontSize = 20f)
+        Spacer(modifier = Modifier.height(50.dp))
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier.fillMaxWidth()
+        )
+        {
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("True")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                onClick = {
+
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text("False")
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GeoQuizPreview(){
+    GeoQuiz2Theme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            GeoQuizScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
 }
